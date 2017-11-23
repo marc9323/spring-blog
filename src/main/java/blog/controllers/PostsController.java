c7044a1755eca456d0a9a7b3f3ef3556c05a7c02
marc9323 <marcholman9323@gmail.com>
2017-11-23 12:33:19 -0600

    first commit

---
+++ src/main/java/blog/controllers/PostsController.java
@@ -0,0 +1,35 @@
+package blog.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import blog.models.Post;
+import blog.services.NotificationService;
+import blog.services.PostService;
+
+@Controller
+public class PostsController {
+	
+	@Autowired
+	private PostService postService;
+	
+	@Autowired
+	private NotificationService notifyService;
+	
+	@RequestMapping("/posts/view/{id}")
+	public String view(@PathVariable("id") Long id, Model model) {
+		Post post = postService.findById(id);
+		if(post == null) {
+			notifyService.addErrorMessage("Cannot find post # " + id);
+			// redirect to the home page where the error message will be shown
+			return "redirect:/";
+		}
+		model.addAttribute("post", post);
+		return "posts/view";
+		
+	}
+
+}
---
+++ src/main/java/blog/services/UserServiceJpaImpl.java
@@ -0,0 +1,46 @@
+package blog.services;
+
+import blog.models.User;
+import blog.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+@Primary
+public class UserServiceJpaImpl implements UserService {
+	@Autowired
+	private UserRepository userRepo;
+
+	@Override
+	public List<User> findAll() {
+		return this.userRepo.findAll();
+	}
+
+	@Override
+	public User findById(Long id) {
+		return this.userRepo.findOne(id);
+	}
+
+	@Override
+	public User create(User user) {
+		return this.userRepo.save(user);
+	}
+
+	@Override
+	public User edit(User user) {
+		return this.userRepo.save(user);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		this.userRepo.delete(id);
+	}
+
+	@Override
+	public boolean authenticate(String username, String password) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
